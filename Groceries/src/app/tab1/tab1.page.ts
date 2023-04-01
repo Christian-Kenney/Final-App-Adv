6371040fcc2d5062f650bc41d898e2e8f79e4041
Christian-Kenney <39739946+Christian-Kenney@users.noreply.github.com>
2023-04-01 01:56:56 -0500

    week 3, add and remove

--- Groceries/src/app/tab1/tab1.page.ts
+++ Groceries/src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,72 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  title = "Grocery";
 
+  items = [
+    {
+      name: "Milk",
+    quantity: 2
+    },
+    {
+      name: "Bread",
+      quantity: 1
+    },
+    {
+      name: "Eggs",
+      quantity: 12
+    }
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {}
+
+  async removeItem(item: any, index: any) {
+    console.log("Removing Item - ", item, index);
+    const toast = await this.toastCtrl.create({
+      message: 'Removing Item - ' + item.name + ' ...',
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1);
+  }
+
+  
+  addItem(){
+    console.log("add item")
+    this.showAddItemPrompt();
+  }
+
+  async showAddItemPrompt(){
+    const prompt = this.alertCtrl.create({
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item)
+            this.items.push(item);
+          }
+        }
+      ]
+      
+    });
+    (await (prompt)).present();
+  }
 }
